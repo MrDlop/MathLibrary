91570dc5d13520b418625912f771895a35638a67
MrDlop <72319098+MrDlop@users.noreply.github.com>
2021-11-19 23:55:02 +0500

    Update Vector2D.cpp

--- Vector2D/Vector2D.cpp
+++ Vector2D/Vector2D.cpp
@@ -1,65 +1,65 @@
-#include "Vector.h"
+#include "Vector2D.h"
 namespace emsp {
-	Vector::Vector(float x, float y)
+	Vector2D::Vector2D(float x, float y)
 	{
 		this->x = x;
 		this->y = y;
 	}
-	Vector::Vector(float length, float a, bool T)
+	Vector2D::Vector2D(float length, float a, bool T)
 	{
 		this->y = sin(a) * length;
 		this->x = cos(a) * length;
 	}
-	Vector::Vector(Vector& v) {
+	Vector2D::Vector2D(Vector2D& v) {
 		this->x = v.x;
 		this->y = v.y;
 	}
-	Vector::Vector() {
+	Vector2D::Vector2D() {
 		this->x = 0;
 		this->y = 0;
 	}
-	void Vector::setLenght(float lenght)
+	void Vector2D::setLenght(float lenght)
 	{
 		double tempL = size() / lenght;
 		this->x *= tempL;
 		this->y *= tempL;
 
 	}
-	void Vector::setA(float a)
+	void Vector2D::setA(float a)
 	{
 		this->y = sin(a) * size();
 		this->x = cos(a) * size();
 	}
-	float Vector::getA()
+	float Vector2D::getA()
 	{
 		return asin(this->y / size());
 	}
-	float Vector::size()
+	float Vector2D::size()
 	{
 		return sqrt(pow(x, 2) + pow(y, 2));
 	}
-	float Vector::mylScalar(Vector v)
+	float Vector2D::mylScalar(Vector2D v)
 	{
 		return v.x * v.y + this->x * this->y;
 	}
 
-	void Vector::mylScalar(int a)
+	void Vector2D::mylScalar(int a)
 	{
 		this->x *= a;
 		this->y *= a;
-	}
-	void Vector::add(Vector v)
+		}
+	void Vector2D::add(Vector2D v)
 	{
 		this->x += v.x;
 		this->y += v.y;
 	}
 
-	void Vector::sub(Vector v)
+	void Vector2D::sub(Vector2D v)
 	{
 		this->x -= v.x;
 		this->y -= v.y;
 	}
-	Vector::~Vector()
+	Vector2D::~Vector2D()
 	{
 	}
 }
